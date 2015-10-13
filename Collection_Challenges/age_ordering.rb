22dd4b68f5b9d0e6b13e5e2a3549c908ed1addfc
ToniRib <tonimarierib@gmail.com>
2015-10-13 17:25:01 -0600

    Adds initial solutions to all array problems in Collection Challenges (for age_ordering)

--- Collection_Challenges/age_ordering.rb
+++ Collection_Challenges/age_ordering.rb
@@ -10,23 +10,28 @@ data = [
 ]
 
 # Level 1: Write code that'll output the names (and only the names) in order by ascending age
+puts 'Level 1:'
 data.sort_by { |name, age| age }.each { |person| puts person[0] }
+puts ""
 
 # Level 2: Output the name with the age, like Juan (24)
+puts 'Level 2:'
 data.sort_by { |name, age| age }.each { |person| puts "#{person[0]} (#{person[1]})" }
+puts ""
 
 # Level 3: If there are multiple people tied with the same age, put them in alphabetical order
-age_sorted = data.sort_by { |name, age| age }
-new_array = []
-age_sorted.each_with_index do |person, idx|
-  # binding.pry
-  if age_sorted.select { |_, age| age == person[1] }.count > 1
-    new_array << age_sorted.select { |_, age| age == person[1] } && ! new_array.include?(person)
-  else
-    new_array << person
-  end
-end
-
-p new_array
+puts 'Level 3:'
+data.sort_by { |n, a| [a, n] }.each { |person| puts "#{person[0]} (#{person[1]})" }
+puts ""
 
 # Level 4: Write code to automatically build a hash with the age as the key and an array of names as the value (all the people who are that age). e.g. {24 => ['Juan', 'Steve', 'Jill']...}
+puts 'Level 4:'
+hash = {}
+data.each do |person|
+  if hash.include?(person[1])
+    hash[person[1]] << person[0]
+  else
+    hash[person[1]] = [person[0]]
+  end
+end
+p hash
