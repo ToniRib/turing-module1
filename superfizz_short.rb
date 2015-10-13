bf55018121cf326b1b49b2aaa3d9b5af575cd2e7
ToniRib <tonimarierib@gmail.com>
2015-10-12 18:24:10 -0600

    First commit for the short version of superfizz. Works, but can probably be shorter.

--- superfizz_short.rb
+++ superfizz_short.rb
@@ -1,10 +1,22 @@
 require 'pry'
 
-TRANSLATE = { 105 => 'SuperFizzBuzz',
-              21 => 'SuperFizz',
-              35 => 'SuperBuzz',
-              3 => 'Fizz',
+TRANSLATE = { 3 => 'Fizz',
               5 => 'Buzz',
               7 => 'Super' }
 
-(0..1000).each { |n| puts n }
+keys = TRANSLATE.keys
+
+(0..1000).to_a.each do |n|
+  vals = keys.select { |k| n % k == 0 }
+  if vals.length == 3
+    puts 'SuperFizzBuzz'
+  elsif vals.include?(3) && vals.include?(7)
+    puts 'SuperFizz'
+  elsif vals.include?(5) && vals.include?(7)
+    puts 'SuperBuzz'
+  elsif vals.length == 1
+    puts TRANSLATE.fetch(vals.first)
+  else
+    puts n
+  end
+end
